98076d48e5b20400f56c7ad94a221c59c30a088c
ellisman31 <akos99@protonmail.com>
2025-06-15 21:46:57 +0200

    Game category ENUM for possible outcomes after rolls

---
+++ src/game/modelEnum/GameCategory.java
@@ -0,0 +1,14 @@
+package game.modelEnum;
+
+public enum GameCategory {
+    SAME_NUMBER_SUM,
+    PAIR,
+    TWO_PAIR,
+    DRILL,
+    POKER,
+    YAHTZEE,
+    LITTLE_ROW,
+    BIG_ROW,
+    FULL,
+    SUM
+}
